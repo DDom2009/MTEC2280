9d20de4e81870c9881840764e4bf1e3c0ae7b121
david dominguez <david.dominguez1@mail.citytech.cuny.edu>
2020-11-03 23:01:51 -0500

    week9 assignment

---
+++ SS6_readygo/readygo/readygo.ino
@@ -0,0 +1,46 @@
+const int buttonPin = 2;     // creates variable called buttonPin and assigns value of 2
+const int LED =  13;      // creates variable called LED and assigns value of 13
+const int LED1 = 12;
+const int LED2 = 11;
+const int LED3 = 10;
+// sets up integer variable that will change over time
+int buttonState = 0;         // creates variable to track buttonState, assigns initial value of 0
+
+
+// setup runs once at startup
+void setup() {
+  pinMode(LED, OUTPUT);  // initialize LED pin as an output.
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(buttonPin, INPUT);  // initialize pushbutton pin as an input.
+}
+
+// loop runs infinitely
+void loop() {
+  buttonState = digitalRead(buttonPin);    // read the state of the pushbutton value
+  delay (10); //small delay for debouncing, to increase accuracy
+
+  // conditional statement
+  if (buttonState == HIGH) { // if button is pressed (voltage received on pin 2)
+    digitalWrite(LED, HIGH);      // send voltage to LED pin
+    delay (1000);
+    digitalWrite(LED1, HIGH);
+    delay (1000);
+    digitalWrite(LED2, HIGH);
+    delay (1000);
+    digitalWrite(LED3, HIGH);
+    delay (1000);
+   
+  } else {   //otherwise
+    digitalWrite(LED, LOW);
+    delay (0);
+    digitalWrite(LED1, LOW);// turn off voltage to LED pin
+    delay (0);
+    digitalWrite(LED2, LOW);
+    delay (0);
+    digitalWrite(LED3, LOW);
+    delay (0);
+  }
+  
+}
